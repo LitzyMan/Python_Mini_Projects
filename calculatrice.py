94f54cdf4cad1f5d64284305963ff7bff2c62b09
Kavitha Gopal <kavithagopal@iMac-de-Kavitha.local>
2023-09-12 19:36:57 +0200

    Create a window

--- calculatrice.py
+++ calculatrice.py
@@ -1,2 +1,11 @@
 from tkinter import *
 
+# create a first window
+window = Tk()
+
+# personnalisez cette fenetre
+window.title("Calculatrice")
+window.minsize(300, 300)
+window.config(background='black')
+# afficher
+window.mainloop()
