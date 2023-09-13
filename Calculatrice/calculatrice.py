1235974c20b91441a1532e32d5437999359555ea
Kavitha Gopal <kavithagopal@iMac-de-Kavitha.local>
2023-09-13 23:38:04 +0200

    Structures the projects

---
+++ Calculatrice/calculatrice.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# create a first window
+window = Tk()
+
+# custom the window
+window.title("Calculatrice")
+window.minsize(600, 600)
+window.config(background='black')
+# display
+window.mainloop()
